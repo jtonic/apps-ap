df3907f34d79e92934038ab12d5e04f65b180417
jtonic <antonel.pazargic@gmail.com>
2015-04-26 01:00:25 +0200

    Bower configuration

--- gulpfile-web.js
+++ gulpfile-web.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+var wiredep = require('wiredep').stream;
 
 gulp.task('jshint', function () {
     gulp.src('./src/scripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bower', function () {
+    gulp.src('./web/public/index.html')
+        .pipe(wiredep({}))
+        .pipe(gulp.dest('./web/public'));
 });
 
 gulp.task('web', function (cb) {
@@ -16,7 +23,7 @@ gulp.task('web', function (cb) {
     })
 });
 
-gulp.task('default', ['web'], function () {
+gulp.task('default', ['bower', 'web'], function () {
 
 });
 
