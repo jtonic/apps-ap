b0e36719f19e90c1e3e058117f0201435fc2d1f7
jtonic <antonel.pazargic@gmail.com>
2015-04-26 21:23:44 +0200

    Fix - incorrect actions for buttons How to run the web application and rest server

--- readme.md
+++ readme.md
@@ -5,3 +5,16 @@
 5. install dependencies
     npm install $dependency --save-dev    
     gulp, jshint, express
+    
+    
+How to run the application:
+1. start the rest server
+    gulp --gulpfile gulpfile-rest.js
+    Note: 
+        The REST API
+        GET:
+        POST: 
+2. start the web application
+    gulp --gulpfile gulpfile-web.js
+
+    APP URL: http://localhost:5000/
--- web/public/js/app.js
+++ web/public/js/app.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
             this.full_name = ko.computed(function () {
                 return this.first_name() + " " + this.last_name();
             }, this);
-            this.register = function() {
+            this.info = function() {
                 entity.fetch(
                     {
                         success: function () {
@@ -40,7 +40,7 @@ $(document).ready(function () {
                     });
                 console.log(JSON.stringify(entity));
             };
-            this.info = function() {
+            this.register = function() {
                 entity.save(
                     {
                         success: function () {
