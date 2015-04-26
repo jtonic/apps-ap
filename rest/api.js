3b7695bd13780ac5b45dd6b4049f63e7e872dc83
jtonic <antonel.pazargic@gmail.com>
2015-04-26 21:03:47 +0200

    - Backbone model server by rest services: GET, POST

--- rest/api.js
+++ rest/api.js
@@ -1,10 +1,42 @@
 'use strict';
 
+var bodyParser = require('body-parser');
 var express = require('express');
+
 var app = express();
 var router = express.Router();
 
-app.get('/', function (rqq, res) {
-    res.status(404).json({"message": "not found!!!"});
+var allowCrossDomain = function(req, res, next) {
+    var allowedHost = [
+        'http://localhost:5000'
+    ];
+
+    if(allowedHost.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+        next();
+    } else {
+        res.send({auth: false});
+    }
+};
+
+app.use(allowCrossDomain);
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.get('/users', function (req, res) {
+    res.status(200).json({
+        first_name: "Liviu",
+        last_name: "Pazargic"
+    });
+});
+app.post('/users', function (req, res) {
+    console.log('First name: ' + req.body.first_name);
+    console.log('Last name: ' + req.body.last_name);
 });
 app.listen(4000);
--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
-    var model = new Backbone.Model({first_name: "Antonel", last_name: "Pazargic"});
+
+    $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+        options.crossDomain ={
+            crossDomain: true
+        };
+        options.xhrFields = {
+            withCredentials: true
+        };
+    });
+
+    var Entity = Backbone.Model.extend({
+        url: function () {
+            return 'http://localhost:4000/users';
+        },
+        defaults: {
+            "first_name": "n/a",
+            "last_name": "n/a"
+        }
+    });
+
+    var entity = new Entity();
 
     var ContactViewModel = kb.ViewModel.extend({
         constructor: function (model) {
@@ -7,13 +27,35 @@ $(document).ready(function () {
             this.full_name = ko.computed(function () {
                 return this.first_name() + " " + this.last_name();
             }, this);
-
             this.register = function() {
-                console.log('Clicked!!!');
-            }
+                entity.fetch(
+                    {
+                        success: function () {
+                            console.log('Success!!!');
+                            console.log(JSON.stringify(entity));
+                        },
+                        error: function () {
+                            console.log('An error has occurred!!!');
+                        }
+                    });
+                console.log(JSON.stringify(entity));
+            };
+            this.info = function() {
+                entity.save(
+                    {
+                        success: function () {
+                            console.log('Successfully saved!!!');
+                        },
+                        error: function () {
+                            console.log('An error has occurred while saving!!!');
+                        }
+                    });
+                console.log(entity)
+            };
         }
     });
 
-    var view_model = new ContactViewModel(model);
+    var view_model = new ContactViewModel(entity);
     kb.applyBindings(view_model, $('#databingind')[0]);
+
 });
